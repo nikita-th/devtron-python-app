17c325d32a9cb8a58a150d14a88925d5a9cd5e73
nikita-th <97900107+nikita-th@users.noreply.github.com>
2024-01-04 23:19:03 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 db_user = 'ctgdevops'
 db_password = 'devops123'
 db_name = 'ctg_devops_ktracker'
-db_host = '34.66.229.117'
+db_host = '35.223.248.99'
 
 db_url = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 db = SQLAlchemy()
